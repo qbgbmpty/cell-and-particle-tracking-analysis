1908d8db0a5fbfdb31ffd1eea3d27b9c4f1c21fb
qbgbmpty <qbgbmpty@gmail.com>
2020-12-28 01:58:01 +0800

    Delete main.m

--- build-videowidget-Desktop_Qt_5_9_0_GCC_64bit-Debug/main.m
+++
@@ -1,5 +0,0 @@
-function main(str,int)
-
-relation_object_file = fopen(['/home/ppcb/Downloads/particle_cell_code/single_test/test.txt'],'w');
-fprintf(relation_object_file,'%s %d',str,int);
-fclose('all');
